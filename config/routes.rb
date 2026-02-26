d412ed42df063aaf89b61f7ddb4a90c70fe4402e
kaiomenes15 <kakacnmendes@gmail.com>
2026-02-26 15:07:36 -0300

    Corrige o erro de duplicidade do devise_for eda nomenclatura da root

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "home#index"
-
-  devise_for :user
+  root "categories#index"
 end
