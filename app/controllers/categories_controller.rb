ab140810aa856cf8d82a1bf1a6624d40c1d0c3cb
kaiomenes15 <kakacnmendes@gmail.com>
2026-03-02 14:27:36 -0300

    altera o cadastro de categorias para considerar o id do usuario presente no token

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,11 @@
 class CategoriesController < ApplicationController
+  before_action :authenticate_user!
+  
   before_action :set_category, only: %i[ show edit update destroy ]
 
   # GET /categories or /categories.json
   def index
-    @categories = Category.all
+    @categories = current_user.categories
   end
 
   # GET /categories/1 or /categories/1.json
@@ -21,7 +23,7 @@ class CategoriesController < ApplicationController
 
   # POST /categories or /categories.json
   def create
-    @category = Category.new(category_params)
+    @category = current_user.categories.build(category_params)
 
     respond_to do |format|
       if @category.save
