5e0538eda5dba716536762293b8b901ab4345d58
kaiomenes15 <kakacnmendes@gmail.com>
2026-03-03 14:20:01 -0300

    corrige a declaracao do enum do status de tarefas para implementar a exclusao de categorias

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,5 @@ class Task < ApplicationRecord
   belongs_to :category
   belongs_to :user
 
-  enum status: { pendente: 0, em_progresso: 1, concluida: 2 }
+  enum :status, { pendente: 0, em_progresso: 1, concluida: 2 }
 end
