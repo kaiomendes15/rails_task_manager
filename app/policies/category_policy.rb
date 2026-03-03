ddb3f1bf6c4cc9032a55b1e9256993be0f0becdd
kaiomenes15 <kakacnmendes@gmail.com>
2026-03-03 14:46:49 -0300

    adiciona as policies para atualizar e excluir categorias

--- app/policies/category_policy.rb
+++ app/policies/category_policy.rb
@@ -1,2 +1,12 @@
 class CategoryPolicy < ApplicationPolicy
-    
+  
+    def update?
+        record.user_id == user.id
+        # so atualiza se for o dono da categoria
+    end
+
+    def destroy?
+        record.user_id == user.id
+        # so deleta se for o dono da categoria
+    end
+end
