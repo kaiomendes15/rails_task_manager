ce0e9a8cb9f560e9abae253a878337435400279f
kaiomenes15 <kakacnmendes@gmail.com>
2026-03-04 15:00:01 -0300

    adiciona o metodo category_must_belong_to_user para validar as categorias que o usuario passa no cadastro de uma task

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,17 @@ class Task < ApplicationRecord
   belongs_to :category
   belongs_to :user
 
-  enum :status, { pendente: 0, em_progresso: 1, concluida: 2 }
+  enum :status, pendente: 0, em_progresso: 1, concluida: 2, default: :pendente
+
+  validate :category_must_belong_to_user
+
+  private
+
+  def category_must_belong_to_user
+     if category.present? && category.user_id != user.id
+       errors.add(:category_id, "Essa categoria não pertence ao usuário.")
+     end
+  end
+
+  
 end
