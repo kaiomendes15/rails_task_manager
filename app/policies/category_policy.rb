6dd3953589dc35815bf72c13cc4e7a240e622348
kaiomenes15 <kakacnmendes@gmail.com>
2026-03-03 21:55:02 -0300

    adiciona a policy de categorias para as rotas de alteracao e retorno de dados unitarios + resolve para as rotas de retorno de multiplos registros

--- app/policies/category_policy.rb
+++ app/policies/category_policy.rb
@@ -1,12 +1,29 @@
 class CategoryPolicy < ApplicationPolicy
-  
-    def update?
-        record.user_id == user.id
-        # so atualiza se for o dono da categoria
+  # NOTE: Up to Pundit v2.3.1, the inheritance was declared as
+  # `Scope < Scope` rather than `Scope < ApplicationPolicy::Scope`.
+  # In most cases the behavior will be identical, but if updating existing
+  # code, beware of possible changes to the ancestors:
+  # https://gist.github.com/Burgestrand/4b4bc22f31c8a95c425fc0e30d7ef1f5
+
+  def show?
+      record.user_id == user.id
+  end
+
+  def update?
+    record.user_id == user.id
+  end
+
+  def destroy?
+    record.user_id == user.id
+  end
+
+  class Scope < ApplicationPolicy::Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      # adicionar aq verificações de roles para usuário admin e normal
+      scope.where(user_id: user.id)
     end
 
-    def destroy?
-        record.user_id == user.id
-        # so deleta se for o dono da categoria
-    end
+    
+  end
 end
