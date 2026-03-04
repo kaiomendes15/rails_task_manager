9fee8b14761fa62fc73ef7c233444825845314e0
kaiomenes15 <kakacnmendes@gmail.com>
2026-03-04 13:31:31 -0300

    adiciona o tratamento de erro para quando usuario tenta ver os detalehs de uma categoria que nao e dele

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,15 @@ class ApplicationController < ActionController::Base
 
   # Adiciona o pundit para controle de autorização
   include Pundit::Authorization
+
+  # ve se essa classe de erro específica vai subir.
+  # se subir, ele cancela a tela de erro 500 e delega para o método privado.
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  def user_not_authorized
+    flash[:alert] = "Acesso negado. Você não tem permissão para realizar esta ação."
+    
+    # redireciona o usuario para a pagina ou raiz
+    redirect_back(fallback_location: root_path)
+  end
 end
