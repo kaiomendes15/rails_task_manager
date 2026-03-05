c3c5bcea79c002efca12da9832f0a46561a9a588
kaiomenes15 <kakacnmendes@gmail.com>
2026-03-05 14:39:45 -0300

    faz a migration para atualizar a model user e adicionar o atributo is_admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_26_175044) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_05_173054) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -39,6 +39,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_26_175044) do
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.boolean "is_admin", default: false
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
